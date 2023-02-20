d18517abc28f7463576dfb59528a206baf19b2b0
sisiayang <sisia1329@gmail.com>
2023-02-20 15:38:26 +0800

    add CNN layer into model

---
+++ model.py
@@ -0,0 +1,13 @@
+from keras import layers
+
+class CNN_with_mask(layers.Layer):
+    def __init__(self, filters, kernel_size, name):
+        super().__init__()
+        self.con_layer = layers.Conv1D(filters=filters, kernel_size=kernel_size, padding='same', name=name)
+
+    def call(self, input, training=None, mask=None):
+        output = self.con_layer(input)
+        return output
+
+    def compute_mask(self, inputs, mask=None):
+        return mask
