fbeea4c41e9bf95c032745b80580bb86e6b45c8e
sisiayang <sisia1329@gmail.com>
2023-04-12 10:34:09 +0800

    update version

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 from keras import layers
 
 class CNN_with_mask(layers.Layer):
-    def __init__(self, filters, kernel_size, name):
+    def __init__(self, filters, kernel_size, strides, name):
         super().__init__()
-        self.con_layer = layers.Conv1D(filters=filters, kernel_size=kernel_size, padding='same', name=name)
+        self.con_layer = layers.Conv1D(filters=filters, kernel_size=kernel_size, strides=strides, padding='same', name=name)
 
     def call(self, input, training=None, mask=None):
         output = self.con_layer(input)
